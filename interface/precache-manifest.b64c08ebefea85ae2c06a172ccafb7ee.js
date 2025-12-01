32e87271571db818701335b22542aab9f535cf2b
Web <vp.vlad00@mail.ru>
2025-12-01 13:02:04 +0300

    Normalize line endings

---
+++ interface/precache-manifest.b64c08ebefea85ae2c06a172ccafb7ee.js
@@ -0,0 +1,434 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f6e3f6c4519681c6a954b96f58222f",
+    "url": "/apple-touch-icon-precomposed.png"
+  },
+  {
+    "revision": "84f6e3f6c4519681c6a954b96f58222f",
+    "url": "/apple-touch-icon.png"
+  },
+  {
+    "revision": "db31b07660c26d938a68dae023ff06a6",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "4dbf211f80c2c3b90b65",
+    "url": "/css/404.9c66ffe2.css"
+  },
+  {
+    "revision": "a1f4bb169b1d5a87ca0e",
+    "url": "/css/app.834c62b4.css"
+  },
+  {
+    "revision": "eb3842f2546d602792b9",
+    "url": "/css/camera.61ab9784.css"
+  },
+  {
+    "revision": "9d1df7bd1765f94364e2",
+    "url": "/css/cameras.045124ab.css"
+  },
+  {
+    "revision": "7a081d0879262bd4a43b",
+    "url": "/css/cameras~console~dashboard~login~notifications~plugins~recordings~settings~start.541892da.css"
+  },
+  {
+    "revision": "ba3b6ae2637b202b6e88",
+    "url": "/css/cameras~console~dashboard~notifications~recordings~settings.2346977c.css"
+  },
+  {
+    "revision": "2795a71e741f1efc1fdf",
+    "url": "/css/camera~cameraFeed~cameras~camview~config~console~dashboard~notifications~plugins~recordings~settings~7e019b2d.25aa1097.css"
+  },
+  {
+    "revision": "9e7762b88c406581ace3",
+    "url": "/css/camera~cameraFeed~cameras~camview~dashboard.d3cadd61.css"
+  },
+  {
+    "revision": "795266abcfb8ab6f867b",
+    "url": "/css/camview.b37fa935.css"
+  },
+  {
+    "revision": "a781f7302829f521915b",
+    "url": "/css/chunk-vendors.7e3be068.css"
+  },
+  {
+    "revision": "b4bff2760c6907475985",
+    "url": "/css/config.b3859411.css"
+  },
+  {
+    "revision": "52cc73b4850935aff1cf",
+    "url": "/css/console.61ee9f56.css"
+  },
+  {
+    "revision": "c66a901b0b7547cd73d4",
+    "url": "/css/dashboard.0fa428b9.css"
+  },
+  {
+    "revision": "ce95f4b86944dca8d990",
+    "url": "/css/login.344e82d8.css"
+  },
+  {
+    "revision": "9135e4cd77a83c6a753a",
+    "url": "/css/notifications.787c70ff.css"
+  },
+  {
+    "revision": "6087032b090b750aef18",
+    "url": "/css/notifications~recordings~settings.73e963a9.css"
+  },
+  {
+    "revision": "19238ab6b8d7400033d6",
+    "url": "/css/plugins.dfb1b59f.css"
+  },
+  {
+    "revision": "75d020793d2ca7834166",
+    "url": "/css/recordings.4c485b9a.css"
+  },
+  {
+    "revision": "bb4c2f7c62b6fdab1bfe",
+    "url": "/css/settings.07467277.css"
+  },
+  {
+    "revision": "5853d9fa24bfd7a448e1",
+    "url": "/css/start.4b11d799.css"
+  },
+  {
+    "revision": "cd74ac2b72c933615650",
+    "url": "/css/utilization.04ff0ded.css"
+  },
+  {
+    "revision": "1e20fc7f50e1153641d641489358aeab",
+    "url": "/img/logo.1e20fc7f.svg"
+  },
+  {
+    "revision": "84ee05288fa725d03bd57590b07f0b95",
+    "url": "/img/logo.84ee0528.png"
+  },
+  {
+    "revision": "f1d93d47ccb260c256ab41d410164187",
+    "url": "/img/logo_animated.f1d93d47.svg"
+  },
+  {
+    "revision": "7a077cad6e81aa1c976c4e919b047e5c",
+    "url": "/img/logo_loading_circle.7a077cad.svg"
+  },
+  {
+    "revision": "bf60d51b3ab8e9ea27beb8a0716861b9",
+    "url": "/img/no_user.bf60d51b.png"
+  },
+  {
+    "revision": "876eae5a8f8686ae6c5b754f1116d441",
+    "url": "/img/splash/apple-splash-1125-2436.png"
+  },
+  {
+    "revision": "be4d96fe14b596076a3fcaa4993960ac",
+    "url": "/img/splash/apple-splash-1136-640.png"
+  },
+  {
+    "revision": "a5c9fc00b72fd75e5f72ce7fa9a14284",
+    "url": "/img/splash/apple-splash-1170-2532.png"
+  },
+  {
+    "revision": "73207791e399e8704c8fd46d781e6bce",
+    "url": "/img/splash/apple-splash-1242-2208.png"
+  },
+  {
+    "revision": "c0931bdd8a824d7c541f8013e653efa6",
+    "url": "/img/splash/apple-splash-1242-2688.png"
+  },
+  {
+    "revision": "df7ad0dd26838507b24a15da6e88717c",
+    "url": "/img/splash/apple-splash-1284-2778.png"
+  },
+  {
+    "revision": "1f8d8553ef87927a2e68afb7fe41591e",
+    "url": "/img/splash/apple-splash-1334-750.png"
+  },
+  {
+    "revision": "8e6f07b19bf463fad6d4c49b2fa5ece9",
+    "url": "/img/splash/apple-splash-1536-2048.png"
+  },
+  {
+    "revision": "9649978b33be4ebffa1ed9dff1ca3af0",
+    "url": "/img/splash/apple-splash-1620-2160.png"
+  },
+  {
+    "revision": "4702482299f182e3a29a51de06c6b2a5",
+    "url": "/img/splash/apple-splash-1668-2224.png"
+  },
+  {
+    "revision": "a6111e203fb7792c6547fc185b180d4d",
+    "url": "/img/splash/apple-splash-1668-2388.png"
+  },
+  {
+    "revision": "e9ff383033e7a0a949b2311be378128e",
+    "url": "/img/splash/apple-splash-1792-828.png"
+  },
+  {
+    "revision": "c9b6917b138f7103fbb9d249a5a130a0",
+    "url": "/img/splash/apple-splash-2048-1536.png"
+  },
+  {
+    "revision": "1627e2b09323ac728c3b6f6331fea8af",
+    "url": "/img/splash/apple-splash-2048-2732.png"
+  },
+  {
+    "revision": "1bff4743ee36ebc216fe7306fa990cda",
+    "url": "/img/splash/apple-splash-2160-1620.png"
+  },
+  {
+    "revision": "9518332d49c1b653dc313a5936a4770f",
+    "url": "/img/splash/apple-splash-2208-1242.png"
+  },
+  {
+    "revision": "d27738bc0435b589fc785e87317db344",
+    "url": "/img/splash/apple-splash-2224-1668.png"
+  },
+  {
+    "revision": "16b4db9e994ab23f3e26acce9c752cdd",
+    "url": "/img/splash/apple-splash-2388-1668.png"
+  },
+  {
+    "revision": "29f11ce0b2b972f3c349df2eb71d3f45",
+    "url": "/img/splash/apple-splash-2436-1125.png"
+  },
+  {
+    "revision": "1b9882694fbddb9979a95c642cafaa2a",
+    "url": "/img/splash/apple-splash-2532-1170.png"
+  },
+  {
+    "revision": "bada3015168c235ce913bc59e1306c41",
+    "url": "/img/splash/apple-splash-2688-1242.png"
+  },
+  {
+    "revision": "f8faff032cf0ab7366bd784879055e5d",
+    "url": "/img/splash/apple-splash-2732-2048.png"
+  },
+  {
+    "revision": "71783f51202b07e4e92bc2e5fd5c5aeb",
+    "url": "/img/splash/apple-splash-2778-1284.png"
+  },
+  {
+    "revision": "2524c8f7346ce09dffa076f7e189154b",
+    "url": "/img/splash/apple-splash-640-1136.png"
+  },
+  {
+    "revision": "4f4862af90d9bb1ccb7ca230e4e7f3de",
+    "url": "/img/splash/apple-splash-750-1334.png"
+  },
+  {
+    "revision": "5178146d0eaaa4e2909e55929bfcefdd",
+    "url": "/img/splash/apple-splash-828-1792.png"
+  },
+  {
+    "revision": "b57a6efb6e46d69646369601c3b0ed9b",
+    "url": "/img/splash/apple-splash-dark-1125-2436.png"
+  },
+  {
+    "revision": "bd4b00de58d7fdc86f5873f0346c0dfa",
+    "url": "/img/splash/apple-splash-dark-1136-640.png"
+  },
+  {
+    "revision": "549c0f711f9a94a31cdd355d360a3826",
+    "url": "/img/splash/apple-splash-dark-1170-2532.png"
+  },
+  {
+    "revision": "62d4e47ece8ff8524dc8e2ceecd76a6e",
+    "url": "/img/splash/apple-splash-dark-1242-2208.png"
+  },
+  {
+    "revision": "8f512c79399539bbf276357b48b7e6b7",
+    "url": "/img/splash/apple-splash-dark-1242-2688.png"
+  },
+  {
+    "revision": "9263abade6ffaf6e97ef44975d78c7ed",
+    "url": "/img/splash/apple-splash-dark-1284-2778.png"
+  },
+  {
+    "revision": "3bc2772b936eeaac9ee6c6ce200c1ff9",
+    "url": "/img/splash/apple-splash-dark-1334-750.png"
+  },
+  {
+    "revision": "f638246cde965d32ad8f9db606487ece",
+    "url": "/img/splash/apple-splash-dark-1536-2048.png"
+  },
+  {
+    "revision": "4bf2e8c41f640463cf5875329cf98e26",
+    "url": "/img/splash/apple-splash-dark-1620-2160.png"
+  },
+  {
+    "revision": "8509404f8e69bd15773bf4b08c6d26af",
+    "url": "/img/splash/apple-splash-dark-1668-2224.png"
+  },
+  {
+    "revision": "80e52fd8e78b31446f1e70061f1121b1",
+    "url": "/img/splash/apple-splash-dark-1668-2388.png"
+  },
+  {
+    "revision": "1d9c26438532e1a6552092beabb21daa",
+    "url": "/img/splash/apple-splash-dark-1792-828.png"
+  },
+  {
+    "revision": "ee67531d74d21cd3dd99a143ccc1ecd4",
+    "url": "/img/splash/apple-splash-dark-2048-1536.png"
+  },
+  {
+    "revision": "2be3d86f950b06747e6c6b891b16490e",
+    "url": "/img/splash/apple-splash-dark-2048-2732.png"
+  },
+  {
+    "revision": "1d33ee7903b96e8753cbc0b4a9e5a6c5",
+    "url": "/img/splash/apple-splash-dark-2160-1620.png"
+  },
+  {
+    "revision": "593fea2c22d1d183b478d5ebeb9fc571",
+    "url": "/img/splash/apple-splash-dark-2208-1242.png"
+  },
+  {
+    "revision": "889fc0271d280320980fc714cbc574ae",
+    "url": "/img/splash/apple-splash-dark-2224-1668.png"
+  },
+  {
+    "revision": "f2ec257884edddb899c73a0a6623304c",
+    "url": "/img/splash/apple-splash-dark-2388-1668.png"
+  },
+  {
+    "revision": "d614b940320c5980f877937b79747821",
+    "url": "/img/splash/apple-splash-dark-2436-1125.png"
+  },
+  {
+    "revision": "ab362cf0518514778d215178ba13f398",
+    "url": "/img/splash/apple-splash-dark-2532-1170.png"
+  },
+  {
+    "revision": "e2c9b4ca9fb05bb5e3669484ce8a29d8",
+    "url": "/img/splash/apple-splash-dark-2688-1242.png"
+  },
+  {
+    "revision": "b713cceef207e9f02f014863f77e5e01",
+    "url": "/img/splash/apple-splash-dark-2732-2048.png"
+  },
+  {
+    "revision": "d2088657ec8a1fdf78655fe5740d9cd5",
+    "url": "/img/splash/apple-splash-dark-2778-1284.png"
+  },
+  {
+    "revision": "588fa622fca2238745e557a475840cde",
+    "url": "/img/splash/apple-splash-dark-640-1136.png"
+  },
+  {
+    "revision": "90209ded2c2746b0a785342922079a1d",
+    "url": "/img/splash/apple-splash-dark-750-1334.png"
+  },
+  {
+    "revision": "7e3b0fa7d56f1fb05cdb4c0b1298e510",
+    "url": "/img/splash/apple-splash-dark-828-1792.png"
+  },
+  {
+    "revision": "84ee05288fa725d03bd57590b07f0b95",
+    "url": "/img/web/logo.png"
+  },
+  {
+    "revision": "4612e4e3982d0a1e9aaffdf3105b1532",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dbf211f80c2c3b90b65",
+    "url": "/js/404.504c9424.js"
+  },
+  {
+    "revision": "a1f4bb169b1d5a87ca0e",
+    "url": "/js/app.4b8abb90.js"
+  },
+  {
+    "revision": "eb3842f2546d602792b9",
+    "url": "/js/camera.8b6c7210.js"
+  },
+  {
+    "revision": "d26fe2d07891325bb438",
+    "url": "/js/cameraFeed.8c3737cd.js"
+  },
+  {
+    "revision": "9d1df7bd1765f94364e2",
+    "url": "/js/cameras.d7837991.js"
+  },
+  {
+    "revision": "7a081d0879262bd4a43b",
+    "url": "/js/cameras~console~dashboard~login~notifications~plugins~recordings~settings~start.d7a8c5ae.js"
+  },
+  {
+    "revision": "ba3b6ae2637b202b6e88",
+    "url": "/js/cameras~console~dashboard~notifications~recordings~settings.c70f0d35.js"
+  },
+  {
+    "revision": "2795a71e741f1efc1fdf",
+    "url": "/js/camera~cameraFeed~cameras~camview~config~console~dashboard~notifications~plugins~recordings~settings~7e019b2d.0fba947c.js"
+  },
+  {
+    "revision": "9e7762b88c406581ace3",
+    "url": "/js/camera~cameraFeed~cameras~camview~dashboard.6bc80901.js"
+  },
+  {
+    "revision": "795266abcfb8ab6f867b",
+    "url": "/js/camview.a0514346.js"
+  },
+  {
+    "revision": "a781f7302829f521915b",
+    "url": "/js/chunk-vendors.03e81977.js"
+  },
+  {
+    "revision": "b4bff2760c6907475985",
+    "url": "/js/config.33104cb2.js"
+  },
+  {
+    "revision": "52cc73b4850935aff1cf",
+    "url": "/js/console.f66694ac.js"
+  },
+  {
+    "revision": "c66a901b0b7547cd73d4",
+    "url": "/js/dashboard.41811018.js"
+  },
+  {
+    "revision": "ce95f4b86944dca8d990",
+    "url": "/js/login.7dc87efa.js"
+  },
+  {
+    "revision": "9135e4cd77a83c6a753a",
+    "url": "/js/notifications.b7dfbcf0.js"
+  },
+  {
+    "revision": "6087032b090b750aef18",
+    "url": "/js/notifications~recordings~settings.0fc4553e.js"
+  },
+  {
+    "revision": "19238ab6b8d7400033d6",
+    "url": "/js/plugins.ddcbc94a.js"
+  },
+  {
+    "revision": "75d020793d2ca7834166",
+    "url": "/js/recordings.dd10f88f.js"
+  },
+  {
+    "revision": "bb4c2f7c62b6fdab1bfe",
+    "url": "/js/settings.1462e391.js"
+  },
+  {
+    "revision": "5853d9fa24bfd7a448e1",
+    "url": "/js/start.df78a662.js"
+  },
+  {
+    "revision": "cd74ac2b72c933615650",
+    "url": "/js/utilization.51a4b60a.js"
+  },
+  {
+    "revision": "b2aa692cc0fe96fa107255917f84e052",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "91f716621fd17c1af19f9e84959592e7",
+    "url": "/media/notification.91f71662.mp3"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
